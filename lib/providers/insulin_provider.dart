12e4b8f8219be9f8ebab920d790d74196fe1e8a8
Nataly-Dev <moreno.nataly404@gmail.com>
2025-08-24 14:02:59 -0600

    update providers
    calculos de los providers

--- lib/providers/insulin_provider.dart
+++ lib/providers/insulin_provider.dart
@@ -22,7 +22,7 @@ class InsulinNotifier extends StateNotifier<InsulinResult?> {
 
   void calculateInverseSingle({required double dose, required double weight}) {
     final factor = double.parse((dose / weight).toStringAsFixed(1));
-    
+
     state = InsulinResult(tdd: dose, factor: factor);
   }
 
@@ -30,19 +30,16 @@ class InsulinNotifier extends StateNotifier<InsulinResult?> {
 
   void calculateBidNph({required double weight, required double dose}) {
     final tdd = weight * dose;
-    final morning = tdd * 0.66;
-    final night = tdd * 0.33;
+    final morning = tdd * (2 / 3);
+    final night = tdd * (1 / 3);
     state = InsulinResult(tdd: tdd, morningDose: morning, nightDose: night);
   }
 
-  void calculateBidNphInverse({
-    required double weight,
-    required double totalDose,
-  }) {
-    final factor = double.parse((totalDose / weight).toStringAsFixed(1));
-    
-    final morning = totalDose * 0.66;
-    final night = totalDose * 0.33;
+  void calculateBidNphInverse({required double weight,required double totalDose,}) {
+    final factor = (totalDose / weight);//double.parse((totalDose / weight).toStringAsFixed(1));
+
+    final morning = totalDose * (2 / 3);
+    final night = totalDose * (1 / 3);
     state = InsulinResult(
       tdd: totalDose,
       morningDose: morning,
@@ -55,15 +52,18 @@ class InsulinNotifier extends StateNotifier<InsulinResult?> {
   void calculateCombinedTx({required double weight, required double dose}) {
     final factor = dose;
     final tdd = weight * dose;
-    final morningDose = tdd * 0.66;
-    final nightDose = tdd * 0.33;
 
-    // Aquí se crea el modelo extendido con dosis NPH y Simple
-    final morningNph = morningDose * 0.66; // ej: 2/3
-    final morningSimple = morningDose * 0.33; // ej: 1/3
+    // AM = 2/3, PM = 1/3
+    final morningDose = tdd * (2 / 3);
+    final nightDose = tdd * (1 / 3);
 
-    final nightNph = nightDose * 0.5;
-    final nightSimple = nightDose * 0.5;
+    // Dentro del desayuno: NPH 2/3, Simple 1/3
+    final morningNph = morningDose * (2 / 3);
+    final morningSimple = morningDose * (1 / 3);
+
+    // Dentro de la cena: mitad y mitad
+    final nightNph = nightDose * (1/2);
+    final nightSimple = nightDose * (1/2);
 
     state = CombinedTxInsulin(
       factor: factor,
@@ -77,13 +77,12 @@ class InsulinNotifier extends StateNotifier<InsulinResult?> {
     );
   }
 
-  void calculateCombinedTxInverse({
-    required double weight,
-    required double totalDose,
-  }) {
-    // modo inverso
-    final dose = (double.parse((totalDose / weight).toStringAsFixed(1)));
-    calculateCombinedTx(weight: weight, dose: dose);
+  void calculateCombinedTxInverse({required double weight,required double totalDose,}) {
+
+    final dose = (totalDose / weight);
+    final roundedDose = dose;
+
+    calculateCombinedTx(weight: weight, dose: roundedDose);
   }
 
   ////////////////////////////////////////////////////////////////////////////////////
@@ -96,13 +95,13 @@ class InsulinNotifier extends StateNotifier<InsulinResult?> {
     final tdd = peso * dosis;
 
     // Distribución de dosis
-    final morningDose = tdd * 0.66;
-    final nightDose = tdd * 0.33;
+    final morningDose = tdd * (2/3);
+    final nightDose = tdd * (1/3);
 
-    final morningNph = morningDose * 0.66;
-    final morningSimple = morningDose * (0.33 / 2);
+    final morningNph = morningDose * (2/3);
+    final morningSimple = morningDose * ((1/3) / 2);
 
-    final lunchSimple = morningDose * (0.33 / 2); // Ejemplo de almuerzo simple
+    final lunchSimple = morningDose * ((1 / 3) / 2); // almuerzo simple
 
     final nightNph = nightDose * 0.5;
     final nightSimple = nightDose * 0.5;
