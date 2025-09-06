38dd407f2e63ce1fa6de295570190364ed3a87e8
Nataly-Dev <moreno.nataly404@gmail.com>
2025-09-05 23:00:43 -0600

    version 12
    cambio de vistas

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -9,11 +9,65 @@ import '../screens/combined_tx_insulin/combined_tx_insulin_screen.dart';
 import '../screens/combined_tx_intensified/combined_tx_intensified_screen.dart';
 import '../screens/single_dose_pm/single_dose_pm_screen.dart';
 import '../screens/login/login_screen.dart';
+import '../screens/treatments/splashScreen.dart';
 
 final appRouter = Provider<GoRouter>((ref) {
   final isLoggedIn = ref.watch(authProvider);
-
   return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/bidNph',
+        builder: (context, state) => const BidNphScreen(),
+      ),
+      GoRoute(
+        path: '/combinedTx',
+        builder: (context, state) => const CombinedTxInsulinScreen(),
+      ),
+      GoRoute(
+        path: '/txIntensified',
+        builder: (context, state) => const CombinedTxItensifiedScreen(),
+      ),
+      GoRoute(
+        path: '/singleDosePm',
+        builder: (context, state) => const SingleDosePmScreen(),
+      ),
+    ],
+    redirect: (context, state) {
+      final location = state.uri.toString();
+
+      // Si estamos en splash, decidir a dónde ir
+      if (location == '/splash') {
+        return isLoggedIn ? '/' : '/login';
+      }
+
+      // Si ya está logueado, no dejar volver a login
+      if (isLoggedIn && location == '/login') {
+        return '/';
+      }
+
+      // Si no está logueado y quiere entrar a cualquier otra ruta, mandarlo a login
+      if (!isLoggedIn && location != '/login') {
+        return '/login';
+      }
+
+      return null;
+    },
+  );
+});
+  /*return GoRouter(
     initialLocation: '/',
     redirect: (context, state) {
       final location = state.uri.toString();
@@ -55,4 +109,4 @@ final appRouter = Provider<GoRouter>((ref) {
       ),
     ],
   );
-});
+});*/
